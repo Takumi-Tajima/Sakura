e0c6d606db07a35acfecb0693fa32371ae4a3a97
taji <taji@sonicgarden.jp>
2025-05-25 18:29:43 +0900

    add user session and registrations spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { '田島 匠' }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'password123' }
+    confirmed_at { Time.current }
   end
 end
