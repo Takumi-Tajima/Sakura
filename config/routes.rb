020ece7e73bd9aae4017d41ea242235565bcb606
taji <taji@sonicgarden.jp>
2025-07-03 08:30:26 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins, controllers: { sessions: 'admins/sessions', passwords: 'admins/passwords' }
 
+  root 'foods#index'
+
   resources :foods, only: %i[index show]
 
   namespace :admins do
@@ -11,7 +13,11 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'foods#index'
+  namespace :users do
+    resource :cart, only: %i[show] do
+      resources :cart_items, only: %i[new create update destroy], module: :carts
+    end
+  end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
