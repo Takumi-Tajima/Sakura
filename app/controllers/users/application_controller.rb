2af7a93178e7dfbe44f303005e14e571ecdcbfe4
taji <taji@sonicgarden.jp>
2025-07-03 16:55:51 +0900

    current_cartを定義

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -2,4 +2,12 @@ class Users::ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
   layout 'application'
+
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    @current_cart ||= current_user.cart || current_user.create_cart!
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  has_one :cart, dependent: :destroy
+
+  def create_cart
+    Cart.create!(user: self)
+  end
 end
